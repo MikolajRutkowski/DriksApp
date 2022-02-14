a02be189fbe6f16e1fab4bd8301a90624af9333c
Mikolaj Rutkowski <mikolaj99x@gmail.com>
2022-02-14 21:25:00 +0100

    Add project files.

---
+++ DriksApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DriksApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void AddNew_Click(object sender, RoutedEventArgs e)
+        {
+
+            Uri newImage = new Uri("/Kamikadze.PNG", UriKind.Relative);
+            DrinkImage.Source = new BitmapImage(newImage);
+        }
+
+        private void Right_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Left_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
