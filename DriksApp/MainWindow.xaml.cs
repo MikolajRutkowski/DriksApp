a67b6315cb82bd88419290ceefeab641af34d5a9
Mikolaj Rutkowski <mikolaj99x@gmail.com>
2022-03-06 12:29:00 +0100

    fix

--- DriksApp/MainWindow.xaml.cs
+++ DriksApp/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace DriksApp
           
         
 
-        private async void  AddNew_Click(object sender, RoutedEventArgs e)
+        private  void  AddNew_Click(object sender, RoutedEventArgs e)
         {
             Uri newImage;
             if (o%2 == 0)
@@ -43,10 +43,7 @@ namespace DriksApp
             else {  newImage = new Uri("/Resorces/Kamikadze.PNG", UriKind.Relative); }
             o++;
             DrinkImage.Source = new BitmapImage(newImage);
-            string text =
-           "A class is the most powerful data type in C#. Like a structure, " +
-           "a class defines the data and behavior of the data type. ";
-            await File.WriteAllTextAsync("/Resorces/Nazwa.txt", text);
+           
         }
 
         private void Right_Click(object sender, RoutedEventArgs e)
@@ -59,7 +56,7 @@ namespace DriksApp
 
         }
 
-        private async void Exit_Button_Click(object sender, RoutedEventArgs e)
+        private  void Exit_Button_Click(object sender, RoutedEventArgs e)
         {
             
         }
