a2898ccc55ca961eb56740a3559f90c722cbcf43
Mikołaj Rutkowski <mikolaj99x@gmail.com>
2022-03-06 12:07:57 +0100

    save to fiel

--- DriksApp/MainWindow.xaml.cs
+++ DriksApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,21 +21,32 @@ namespace DriksApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        static int o = 1;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
+       
+           
 
-        private void AddNew_Click(object sender, RoutedEventArgs e)
-        {
+          
+        
 
-            Uri newImage = new Uri("/Kamikadze.PNG", UriKind.Relative);
+        private async void  AddNew_Click(object sender, RoutedEventArgs e)
+        {
+            Uri newImage;
+            if (o%2 == 0)
+            {
+             newImage = new Uri("/Resorces/hhh.jpg", UriKind.Relative);
+            }
+            else {  newImage = new Uri("/Resorces/Kamikadze.PNG", UriKind.Relative); }
+            o++;
             DrinkImage.Source = new BitmapImage(newImage);
+            string text =
+           "A class is the most powerful data type in C#. Like a structure, " +
+           "a class defines the data and behavior of the data type. ";
+            await File.WriteAllTextAsync("/Resorces/Nazwa.txt", text);
         }
 
         private void Right_Click(object sender, RoutedEventArgs e)
@@ -45,6 +57,11 @@ namespace DriksApp
         private void Left_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private async void Exit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
