1db4073e96f10eccd7027babda24fe640662293d
Mikolaj Rutkowski <mikolaj99x@gmail.com>
2022-06-13 21:50:33 +0200

    almoust done

--- DriksApp/MainWindow.xaml.cs
+++ DriksApp/MainWindow.xaml.cs
@@ -28,26 +28,32 @@ namespace DriksApp
        public static int Counter = 0;
         static int MaxCounter = 0;
 
-       
-
-      
-       
 
         static string[] tab;
-        public int FindMaxCounter(string entranse = @"C:\Users\mikol\source\repos\DriksApp\DriksApp\Resorces\Nazwa.txt")
+        public int FindMaxCounter(string entranse = @"C:\Users\mikol\source\repos\DriksApp\DriksApp\Resorces\Nazwa.txt", char stoper = '\n', bool normal = true)
         {
+            string readText;
+            if (normal)
+            {
             string path = entranse;
-            string readText = File.ReadAllText(path);
+            readText = File.ReadAllText(path);
+            }
+            else
+            {
+                readText = entranse;
+            }
+            
             int maxcounter = 0;
             foreach (char c in readText)
             {
-                if (c == '\n')
+                if (c == stoper )
                 {
                     maxcounter++;
                 }
             }
             return maxcounter;
         }
+        
 
        public string Cut(int x , string entrance, char c = '\n')
         {
@@ -88,16 +94,17 @@ namespace DriksApp
         {
             string path = @"C:\Users\mikol\source\repos\DriksApp\DriksApp\Resorces\Nazwa.txt";
             string readText = File.ReadAllText(path);
-
+            
             MaxCounter = FindMaxCounter() + 1;
             tab = new string[MaxCounter];
             InitializeComponent();
             for (int i = 0; i < tab.Length; i++)
             {
-
+               
                 tab[i] = Cut(i,readText);
             }
             MaxCounter--;
+            Right_Click();
             
         }
 
@@ -118,13 +125,68 @@ namespace DriksApp
 
         public  BitmapImage SetImage(string patch)
         {
-            Uri newImage;
-            //"/Resorces/hhh.jpg"
+            Uri newImage;       
             newImage = new Uri(patch, UriKind.Relative);
             
             return new BitmapImage(newImage); 
         }
 
+        public int FindNumber(string target)
+        {
+            for (int i = 0; i < target.Length; i++)
+            {
+                if(target[i] == '1'|| target[i] == '2' || target[i] == '3' || target[i] == '4' || target[i] == '5' || target[i] == '6' || target[i] == '7' || target[i] == '8' || target[i] == '9')
+                {
+                    return i;
+                }
+            }
+            return 0;
+        } 
+        public bool IsPossibleToMake(string igrediens)
+        {
+            int fucktymczas = 2;
+            IsOkToMake.Text = " ";
+            int CounterOfIgredniens = FindMaxCounter(igrediens,'.',false);
+            string[] tab  = new string[CounterOfIgredniens];
+            
+            for (int i = 0; i < CounterOfIgredniens; i++)
+            {
+                string x = Cut(i, igrediens, '.');
+                tab[i] = x;
+            }
+            IsOkToMake.Text += tab[fucktymczas];
+            for (int i = 0; i < CounterOfIgredniens; i++)
+            {
+                if (i == 0)
+                {
+
+                }
+                else
+                {
+                    tab[i] = tab[i].Remove(0, 2);
+                }
+                
+                int end = FindMaxCounter(tab[i], ' ', false);
+                tab[i] = tab[i].Remove(tab[i].Length  - end +1 );
+            }
+            string[] tab2 = new string[CounterOfIgredniens];
+            int[] intTab = new int[CounterOfIgredniens];
+            IsOkToMake.Text += tab[fucktymczas];
+
+            for (int i = 0; i < CounterOfIgredniens; i++)
+            {
+                int x = FindNumber(tab[i]);
+                for (int k = 0; k < x; k++)
+                {
+                    string kk =   tab[i].Substring(x);
+                    kk.Remove(2);
+                    intTab[i] = int.Parse(kk);
+                }
+            }
+          
+            IsOkToMake.Text +=   tab2[fucktymczas];
+            return false;
+        }
         public void SetAll()
         {
             DrinkNumber.Text = "Drink nr. " + SetText(tab,0);
@@ -132,6 +194,11 @@ namespace DriksApp
             DrinkImage.Source = SetImage(SetText(tab,2));          
             Ingredients.Text = SetText(tab,3);
             Instruction.Text = SetText( tab ,4);
+            if(IsPossibleToMake(SetText(tab, 3)))
+            {
+
+                Gri.Background = Brushes.Blue;
+            }
 
         }
 
@@ -153,6 +220,16 @@ namespace DriksApp
             }
             SetAll();
         }
+        private void Right_Click()
+        {
+            Counter++;
+            if (Counter == tab.Length)
+            {
+                Counter = 1;
+            }
+            SetAll();
+        }
+
 
         private void Left_Click(object sender, RoutedEventArgs e)
         {
